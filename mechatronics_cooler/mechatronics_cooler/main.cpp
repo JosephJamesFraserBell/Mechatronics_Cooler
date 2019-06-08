1b32cb1997927f8d960a624c8392a89f7e5e6dcf
JosephJamesFraserBell <44582108+JosephJamesFraserBell@users.noreply.github.com>
2019-06-08 15:50:56 -0400

    First commit - blank c file
    First commit of blank c file for project to test repository

---
+++ mechatronics_cooler/mechatronics_cooler/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * mechatronics_cooler.cpp
+ *
+ * Created: 6/8/2019 3:50:18 PM
+ * Author : josep
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
